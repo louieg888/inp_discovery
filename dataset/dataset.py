weighting correct, fixed metric, model training appropriately

dataset/dataset.py
@@ -202,10 +202,8 @@ class NuRD(Dataset):
                 data = generate_synthetic_data(-0.9, m=2000)
 
             x, y, z = data
-            self.data = pd.DataFrame({"x": x, "y": y, "knowledge": z})
+            self.data = pd.DataFrame({"x": [list(el.numpy()) for el in x], "y": y, "z": z})
             self.data['task'] = np.floor(self.data.index / 100).astype(int)
-
-            import pdb; pdb.set_trace()
 
         self.dim_x = 2
         self.dim_y = 1

models/inp.py
@@ -26,7 +26,8 @@ class SyntheticBernoulliReweightingModel(nn.Module):
         hidden_1 = F.relu(self.hidden_1(z))
         # Apply the output layer and sigmoid activation to get probabilities
         hidden_2 = self.hidden_2(hidden_1)
-        prediction = self.sigmoid(self.pred_layer(hidden_2))
+        # prediction = self.sigmoid(self.pred_layer(hidden_2))
+        prediction = self.pred_layer(hidden_2)
 
         return prediction
 

models/train_reweighting.py
@@ -40,7 +40,7 @@ class Trainer:
 
         self.reweighting_model = SyntheticBernoulliReweightingModel()
         self.reweighting_model.to(self.device)
-        self.reweighting_optimizer = torch.optim.Adam(self.reweighting_model.parameters(), lr=1)
+        self.reweighting_optimizer = torch.optim.Adam(self.reweighting_model.parameters(), lr=1e-2)
         
         self.loss_func = ELBOLoss(beta=config.beta)
         if load_path is not None: